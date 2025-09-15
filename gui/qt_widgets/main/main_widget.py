8d7be5bd0f217c2b32e69ca09e5a76e8da260a18
lips <lips137027@qq.com>
2025-09-15 16:49:01 +0800

    gui区分qt_widgets和qml

--- gui/qt_widgets/main/main_widget.py
+++ gui/qt_widgets/main/main_widget.py
@@ -9,7 +9,7 @@ class MainWidget(QWidget):
         
         # 加载 UI 文件，第二个参数 self 表示将控件加载到当前窗口
         # 注意：PyQt5 在加载 .ui文件时，如果发现槽函数名称符合 on_对象名_信号名的格式，​​会自动连接​​信号和槽
-        uic.loadUi('./gui/main/MainWidget.ui', self)  # 确保路径正确
+        uic.loadUi('./gui/qt_widgets/main/MainWidget.ui', self)  # 确保路径正确
 
         # 连接信号槽 (示例：假设UI文件中有一个名为 pushButton 的按钮)
         # 全量获取
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import sys
 from PyQt5.QtCore import QT_VERSION_STR, PYQT_VERSION_STR
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
-from gui.main.main_widget import MainWidget
+from gui.qt_widgets.main.main_widget import MainWidget
 from resources import resources_rc
 
 def main():
     # PyQt5
     app = QApplication(sys.argv)  # 创建应用程序对象[1,7]
-    app.setWindowIcon(QIcon(":/icon/general/close_normal.svg"))
+    app.setWindowIcon(QIcon(":/app.svg"))
     # widget = QWidget()           # 创建窗口实例
     widget = MainWidget()
     widget.setWindowTitle("MPolicy")
