2af5946b04814a455abe2367503f8ca2214338e5
lips <lips137027@qq.com>
2025-09-15 16:30:15 +0800

    添加资源文件；添加UI设计规范文件

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
 from PyQt5.QtCore import QT_VERSION_STR, PYQT_VERSION_STR
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from gui.main.main_widget import MainWidget
+from resources import resources_rc
 
 def main():
     # PyQt5
     app = QApplication(sys.argv)  # 创建应用程序对象[1,7]
+    app.setWindowIcon(QIcon(":/icon/general/close_normal.svg"))
     # widget = QWidget()           # 创建窗口实例
     widget = MainWidget()
+    widget.setWindowTitle("MPolicy")
     widget.show()                  # 显示窗口[1,7]
     sys.exit(app.exec_())          # 进入主事件循环[1,7]
 
