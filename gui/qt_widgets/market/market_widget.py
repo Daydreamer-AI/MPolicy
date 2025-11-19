71fc2ab7bbc1404eb56f70122ece1770d1c600a6
lips <lips137027@qq.com>
2025-11-19 15:57:31 +0800

    优化副图指标添加方式

--- gui/qt_widgets/market/market_widget.py
+++ gui/qt_widgets/market/market_widget.py
@@ -9,10 +9,11 @@ import datetime
 from common.logging_manager import get_logger
 
 from gui.qt_widgets.MComponents.candlestick_item import CandlestickItem
-from gui.qt_widgets.MComponents.volume_item import VolumeItem
+
 import numpy as np
 import pyqtgraph as pg
 from gui.qt_widgets.MComponents.custom_date_axisItem import CustomDateAxisItem, NoLabelAxis
+from gui.qt_widgets.market.volume_widget import VolumeWidget
 
 class MarketWidget(QWidget):
     def __init__(self, parent=None):
@@ -26,7 +27,7 @@ class MarketWidget(QWidget):
 
     def init_para(self):
         self.logger = get_logger(__name__)
-        self.other_indicator_widgets = {} 
+        self.indicator_widgets = {} 
 
         # self.df_data列结构：date, code, open, high, low, close, volume, amount, change_percent, turnover_rate, adjustflag, diff, dea, macd, ma5, ma10, ma20, ma24, ma30, ma52, ma60, volume_ratio
         self.df_data = BaoStockProcessor().get_daily_stock_data('sh.600000')    
@@ -51,29 +52,15 @@ class MarketWidget(QWidget):
         widget_k_line_layout.addWidget(self.kline_plot_widget)
 
 
-        volume_widget_layout = self.widget_volume.layout()
-        if volume_widget_layout is None:
-            self.widget_volume.setLayout(QVBoxLayout())
-        self.volume_plot_widget = pg.PlotWidget()
-        self.volume_plot_widget.hideAxis('bottom')
-        self.volume_plot_widget.getAxis('left').setWidth(60)
-        self.volume_plot_widget.setBackground('w')
-        self.volume_plot_widget.showGrid(x=True, y=True)
-        self.volume_plot_widget.setMouseEnabled(x=True, y=False)
-        volume_widget_layout.addWidget(self.volume_plot_widget)
-
-        self.volume_plot_widget.setXLink(self.kline_plot_widget)
-
-
     def init_connect(self):
-        pass
+        self.btn_indicator_volume.clicked.connect(self.slot_btn_indicator_volume_clicked)
 
     def draw_charts(self):
         # ."绘制所有图表…
         self.draw_kline()
-        self.draw_volume()
+        # self.draw_volume()
         # self.draw_macd()
-        self.update_info_labels()
+        # self.update_info_labels()
 
 
     def draw_kline(self):
@@ -108,48 +95,22 @@ class MarketWidget(QWidget):
         self.kline_plot_widget.getAxis('bottom').setTextPen(QtGui.QColor(110, 110, 110))
 
     def draw_volume(self):
-        # ""绘制交易量图
-        self.volume_plot_widget.clear()
+        volume_widget = VolumeWidget(self.df_data, self)
+        return volume_widget
 
-        # 检查是否有数据
-        if self.df_data is None or self.df_data.empty:
-            return
-        
-        # 确保数据列存在
-        required_columns = ['open', 'close', 'volume']
-        if not all(col in self.df_data.columns for col in required_columns):
-            self.logger.warning("缺少必要的数据列来绘制K线图")
-            return
-
-        # 创建交易量图
-        vol_item = VolumeItem(self.df_data)
-
-        self.volume_plot_widget.addItem(vol_item)
-
-        #设置坐标范围
-        self.volume_plot_widget.setXRange(-1, len(self.df_data) + 1, padding=0)
-        max_vol = np.max(self.df_data['volume'])
-        self.volume_plot_widget.setYRange(0, max_vol * 1.1, padding=0)
-
-        #设置坐标轴颜色
-        self.volume_plot_widget.getAxis('left').setPen(QtGui.QColor(110, 110, 110))
-        self.volume_plot_widget.getAxis('bottom').setPen(QtGui.QColor(110, 110, 110))
-        self.volume_plot_widget.getAxis('left').setTextPen(QtGui.QColor(110, 110, 110))
-        self.volume_plot_widget.getAxis('bottom').setTextPen(QtGui.QColor(110, 110, 110))
-
-    def draw_amount(self, plot_widget):
+    def draw_amount(self):
         pass
 
-    def draw_macd(self, plot_widget):
+    def draw_macd(self):
         pass
 
-    def draw_kdj(self, plot_widget):
+    def draw_kdj(self):
         pass
 
-    def draw_rsi(self, plot_widget):
+    def draw_rsi(self):
         pass
 
-    def draw_boll(self, plot_widget):
+    def draw_boll(self):
         pass
 
     def update_info_labels(self):
@@ -160,65 +121,71 @@ class MarketWidget(QWidget):
             动态添加指标图
         '''
         # 先检查是否支持该指标
-        supported_indicators = ['成交额', 'MACD', 'KDJ', 'RSI', 'BOLL']
+        supported_indicators = ['成交量', '成交额', 'MACD', 'KDJ', 'RSI', 'BOLL']
         if indicator_name not in supported_indicators:
             self.logger.warning(f"不支持的指标：{indicator_name}")
             return None
         
         # 检查是否已经添加了该指标
-        if indicator_name in self.other_indicator_widgets:
+        if indicator_name in self.indicator_widgets:
             self.logger.info(f"指标 {indicator_name} 已经存在")
             return self.indicator_widgets[indicator_name]
 
-        plot_widget = pg.PlotWidget()
-        plot_widget.hideAxis('bottom')
-        plot_widget.getAxis('left').setWidth(60)
-        plot_widget.setBackground('w')
-        plot_widget.showGrid(x=True, y=True)
-        plot_widget.setMouseEnabled(x=True, y=False)
-        plot_widget.setXLink(self.kline_plot_widget)
-
-        if indicator_name == '成交额':
-            self.draw_amount(plot_widget)
+        indicator_widget = None
+        if indicator_name == '成交量':
+            indicator_widget = self.draw_volume()
+        elif indicator_name == '成交额':
+            indicator_widget = self.draw_amount()
         elif indicator_name == 'MACD':
-            self.draw_macd(plot_widget)
+            indicator_widget = self.draw_macd()
         elif indicator_name == 'KDJ':
-            self.draw_kdj(plot_widget)
+            indicator_widget = self.draw_kdj()
         elif indicator_name == 'RSI':
-            self.draw_rsi(plot_widget)
+            indicator_widget = self.draw_rsi()
         elif indicator_name == 'BOLL':
-            self.draw_boll(plot_widget)
+            indicator_widget = self.draw_boll()
         else:
             self.logger.warning(f"不支持的指标：{indicator_name}")
-            # 释放PlotWidget
 
-        self.verticalLayout_2.addWidget(plot_widget, 1)
+        # 检查是否成功创建了widget
+        if indicator_widget is None:
+            self.logger.warning(f"无法创建指标 {indicator_name} 的图表")
+            return None
+
+         # 设置图表属性以保持一致性
+        if hasattr(indicator_widget, 'get_plot_widget'):
+            plot_widget = indicator_widget.get_plot_widget()
+            if plot_widget:
+                plot_widget.getAxis('left').setWidth(60)
+                plot_widget.setXLink(self.kline_plot_widget)
+        
+        self.verticalLayout_2.addWidget(indicator_widget, 1)
 
         # 保存图表引用
-        self.other_indicator_widgets[indicator_name] = plot_widget
-        return plot_widget
+        self.indicator_widgets[indicator_name] = indicator_widget
+        return indicator_widget
 
     def remove_indicator_chart(self, indicator_name):
         '''
             移除动态添加的指标图
         '''
         # 检查指标是否存在
-        if indicator_name not in self.other_indicator_widgets:
+        if indicator_name not in self.indicator_widgets:
             self.logger.warning(f"指标 {indicator_name} 不存在")
             return False
 
         # 获取要移除的widget
-        plot_widget = self.other_indicator_widgets[indicator_name]
+        widget = self.indicator_widgets[indicator_name]
         
         # 从布局中移除
-        self.verticalLayout_2.removeWidget(plot_widget)
+        self.verticalLayout_2.removeWidget(widget)
         
         # 隐藏并删除widget
-        plot_widget.setParent(None)
-        plot_widget.deleteLater()
+        widget.setParent(None)
+        widget.deleteLater()
         
         # 从字典中移除引用
-        del self.other_indicator_widgets[indicator_name]
+        del self.indicator_widgets[indicator_name]
         
         self.logger.info(f"成功移除指标 {indicator_name}")
         return True
@@ -228,9 +195,18 @@ class MarketWidget(QWidget):
             移除所有动态添加的指标图
         '''
         # 创建指标名称列表的副本，因为我们在迭代过程中会修改原字典
-        indicator_names = list(self.other_indicator_widgets.keys())
+        indicator_names = list(self.indicator_widgets.keys())
         
         for indicator_name in indicator_names:
             self.remove_indicator_chart(indicator_name)
         
         self.logger.info("成功移除所有指标图表")
+
+
+    # ----------------------槽函数-------------------------
+    def slot_btn_indicator_volume_clicked(self):
+        is_checked = self.btn_indicator_volume.isChecked()
+        if is_checked:
+            self.add_indicator_chart('成交量')
+        else:
+            self.remove_indicator_chart('成交量')
--- gui/qt_widgets/market/volume_widget.py
+++ gui/qt_widgets/market/volume_widget.py
@@ -2,19 +2,93 @@ from PyQt5 import QtWidgets, uic, QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout
 from PyQt5.QtCore import pyqtSlot
 
+import pyqtgraph as pg
+import numpy as np
+
+from common.logging_manager import get_logger
+
+from gui.qt_widgets.MComponents.volume_item import VolumeItem
+
 class VolumeWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, data, parent=None):
         super(VolumeWidget, self).__init__(parent)
 
-        self.init_para()
+        self.init_para(data)
         self.init_ui()
         self.init_connect()
 
     def init_ui(self):
-        pass
+        uic.loadUi('./gui/qt_widgets/market/VolumeWidget.ui', self)
 
-    def init_para(self):
-        pass
+        layout = self.layout()
+        if layout is None:
+            self.logger.info("没有布局，创建一个")
+            self.setLayout(QVBoxLayout())
+            layout = self.layout()
+
+        self.plot_widget = pg.PlotWidget()
+        self.plot_widget.hideAxis('bottom')
+        self.plot_widget.getAxis('left').setWidth(60)
+        self.plot_widget.setBackground('w')
+        self.plot_widget.showGrid(x=True, y=True)
+        self.plot_widget.setMouseEnabled(x=True, y=False)
+        layout.addWidget(self.plot_widget)
+
+        self.draw()
+
+    def init_para(self, data):
+        # 检查是否有数据
+        if data is None or data.empty:
+            raise ValueError("数据为空，无法绘制成交量")
+        
+        # 确保数据列存在
+        required_columns = ['open', 'close', 'volume']
+        if not all(col in data.columns for col in required_columns):
+            self.logger.warning("缺少必要的数据列来绘制成交量")
+            raise ValueError("缺少必要数据列")
+        
+        self.df_data = data
+        self.logger = get_logger(__name__)
 
     def init_connect(self):
         pass
+
+    def update_data(self, data):
+        self.df_data = data
+        self.draw()
+
+    def get_data(self):
+        return self.df_data
+    
+    def get_plot_widget(self):
+        return self.plot_widget
+
+    def draw(self):
+        # ""绘制交易量图
+        self.plot_widget.clear()
+
+        # 检查是否有数据
+        if self.df_data is None or self.df_data.empty:
+            return
+        
+        # 确保数据列存在
+        required_columns = ['open', 'close', 'volume']
+        if not all(col in self.df_data.columns for col in required_columns):
+            self.logger.warning("缺少必要的数据列来绘制成交量")
+            return
+
+        # 创建交易量图
+        vol_item = VolumeItem(self.df_data)
+
+        self.plot_widget.addItem(vol_item)
+
+        #设置坐标范围
+        self.plot_widget.setXRange(-1, len(self.df_data) + 1, padding=0)
+        max_vol = np.max(self.df_data['volume'])
+        self.plot_widget.setYRange(0, max_vol * 1.1, padding=0)
+
+        #设置坐标轴颜色
+        self.plot_widget.getAxis('left').setPen(QtGui.QColor(110, 110, 110))
+        self.plot_widget.getAxis('bottom').setPen(QtGui.QColor(110, 110, 110))
+        self.plot_widget.getAxis('left').setTextPen(QtGui.QColor(110, 110, 110))
+        self.plot_widget.getAxis('bottom').setTextPen(QtGui.QColor(110, 110, 110))
